87c9228775d9daf414dbaa0a23e44b389811ee19
deaja <deaja214@gmail.com>
2021-10-17 04:50:08 -0400

    pseudocode

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,33 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution42 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Solution42 app = new Solution42();
+        //open file
+        Scanner file = new Scanner(new File("data/exercise42_input.txt"));
+        ArrayList<String> employee = new ArrayList<>();
+        /*loop through file use commas to separate
+            first name, last name, and salary add to array list*/
+        //call printNames
+
+        //close input file
+    }
+    private void printNames(ArrayList<String> employee){
+        //array length int
+
+        //print header
+
+        //print array value
+        //new line every 3 values
+    }
+}
