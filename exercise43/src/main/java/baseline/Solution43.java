3b3e8491b2e7a08d9712b90be2a373e9e7e742c6
deaja <deaja214@gmail.com>
2021-10-17 07:20:03 -0400

    psudocode

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,34 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+//create actual files or just print the desired output?
+
+
+public class Solution43 {
+    public static void main(String[] args){
+        //create scanner
+
+        //prompt user
+
+        //call fileMaker
+
+        //call Html
+
+        //call printIfCreated
+    }
+    private void FileMaker(){
+        // use FileWriter to create file
+        //make file - user
+        // if user answered y or Y to using js
+        //file - user - js
+        // if user answered y or Y to using CSS
+        //file - user - css
+    }
+    private void Html(){
+        //create file - user - html
+        //list.write site name
+        //list.write author
+    }
+}
