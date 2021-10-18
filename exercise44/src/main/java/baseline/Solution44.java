1198cdda981a8a9cc52db3d46ebf483d4656d106
deaja <deaja214@gmail.com>
2021-10-17 20:51:44 -0400

    psuedocode

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,20 @@
+package baseline;
+
+public class Solution44 {
+    public static void main(String[] args){
+        //create JSON object
+        //create JSON Array
+        //prompt user
+        //buffer read user input
+        //call parserMethod
+    }
+    private void parserMethod(){
+        // create iterator
+        // loop through iterator
+        // create JSON object
+        // JSON .get from array
+        // if input to lower case = array item
+        //print name, price, quantity
+        //exception catch print item not found
+    }
+}
