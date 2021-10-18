06b0570e0879e56fdd6b80924c6ee76e26a21d61
deaja <deaja214@gmail.com>
2021-10-17 22:04:05 -0400

    testing & uml

--- exercise41/src/test/java/baseline/Solution41Test.java
+++ exercise41/src/test/java/baseline/Solution41Test.java
@@ -2,10 +2,30 @@ package baseline;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class Solution41Test {
+class Solution41Test{
     @Test
-    void main() {
+    void main() throws FileNotFoundException {
+        //create scanner
+        Scanner file = new Scanner(new File("data/exercise41_input.txt"));
+        ArrayList<String> names = new ArrayList<>();
+        //File results = new File("resources/testingData.txt");
+        while(file.hasNextLine()){
+            names.add(file.nextLine());
+        }
+        //use java built-in sort function to sort list
+        names.sort(String.CASE_INSENSITIVE_ORDER);
+        Scanner results = new Scanner(new File("data/testingData.txt"));
+        ArrayList<String> list = new ArrayList<>();
+        while(results.hasNextLine()){
+            list.add(results.nextLine());
+        }
+            assertEquals(names, list);
     }
 }
--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -5,30 +5,71 @@ package baseline;
  */
 //create actual files or just print the desired output?
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Solution43 {
-    public static void main(String[] args){
+    private File jsFile;
+    private File cssFile;
+
+    public static void main(String[] args) throws IOException {
+        Solution43 app = new Solution43();
         //create scanner
-
+        Scanner userIn = new Scanner(System.in);
         //prompt user
-
+        System.out.print("Site name: ");
+        String siteName = userIn.nextLine();
+        System.out.print("Author: ");
+        String author = userIn.nextLine();
+        System.out.print("Do you want a folder for JavaScript? ");
+        char js = userIn.next().charAt(0);
+        System.out.print("Do you want a folder for CSS? ");
+        char css = userIn.next().charAt(0);
         //call fileMaker
-
+        app.fileMaker(siteName, js, css);
         //call Html
+        app.html(siteName, author);
+    }
 
-        //call printIfCreated
-    }
-    private void FileMaker(){
+    private void fileMaker(String site, char js, char css) {
         // use FileWriter to create file
-        //make file - user
+        // make file - site name
+
+        File file = new File(("data/./website/" + site));
+        if(file.exists() && file.isDirectory()){
+            System.out.println("Created " + file);
+        }
         // if user answered y or Y to using js
-        //file - user - js
+        // file - site - js
+        if (js == 'y' || js == 'Y')
+            jsFile = new File(("data/./website/js/"));
+        if(jsFile.exists() && jsFile.isDirectory()){
+            System.out.println("Created " +jsFile);
+        }
         // if user answered y or Y to using CSS
-        //file - user - css
+        //file - site - css
+        if (css == 'y' || css == 'Y')
+            cssFile = new File(("data/./website/css/"));
+        if(cssFile.exists() && cssFile.isDirectory()){
+            System.out.println("Created " +cssFile);
+        }
     }
-    private void Html(){
+    private void html(String site, String auth) throws IOException {
         //create file - user - html
-        //list.write site name
-        //list.write author
+        File htmlFile = new File(("data/./website/" + site + "/index.html"));
+        try (FileWriter hFile = new FileWriter(("data/./website/" + site + "/index.html"))) {
+            //list.write site name
+            hFile.write("<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n");
+            hFile.write("\t<title>" + site + "</title>\n");
+            //list.write author
+            hFile.write("\t<meta author=\"" + auth + "\">\n");
+
+        }
+        if(htmlFile.exists() && htmlFile.isDirectory()){
+            System.out.println("Created " +cssFile);
+        }
+
     }
 }
