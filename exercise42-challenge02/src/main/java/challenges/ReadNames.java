a96c702186766287d70c16f88654b38d2c776b28
deaja <deaja214@gmail.com>
2021-10-19 18:54:29 -0400

    oop principles

---
+++ exercise42-challenge02/src/main/java/challenges/ReadNames.java
@@ -0,0 +1,43 @@
+package challenges;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReadNames {
+    void readNames(BufferedReader file, ArrayList<String> employee) throws IOException {
+        String temp;
+        /*loop through file use commas to separate
+            first name, last name, and salary add to array list*/
+        while((temp = file.readLine()) !=null){
+            String[] comma = temp.split(",");
+            String money = comma[2];
+            String s1 = money.substring(0,2);
+            String s2 = money.substring(2,5);
+            money= "$"+s1+","+s2;
+            employee.add(comma[0]);
+            employee.add(comma[1]);
+            employee.add(money);
+
+        }
+    }
+    void printNames(ArrayList<String> employee){
+        int j = 1;
+        //print header
+        System.out.println("Last\tFirst\tSalary\n--------------------------");
+        //print array values
+        // new line every 3 values
+        for (String s : employee) {
+
+            System.out.printf("%-10s", s);
+            if (j == 3) {
+                System.out.print("\n");
+                j = 1;
+            } else {
+                j++;
+            }
+        }
+
+
+    }
+}
