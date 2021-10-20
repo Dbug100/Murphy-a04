ed78d4b0a4c3a915d43f2e1e364a78be3e8e616e
deaja <deaja214@gmail.com>
2021-10-19 22:57:05 -0400

    psuedocode

---
+++ exercise45/src/main/java/baseline/FileChange.java
@@ -0,0 +1,26 @@
+package baseline;
+
+public class FileChange {
+    //ArrayList
+
+    private void fileRead(){
+        //Scanner
+        //open file
+        // while file has next line add to array
+        //close file
+
+    }
+    private void fileChange(String fileName){
+        // create file for output
+        //create string "utilize"
+        //temp string
+        //for loop size of array
+        //if array i = utilize
+        //add word "use to new array
+        //else
+        // add array i to new array
+        //write array to file
+        //close file
+
+    }
+}
---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,15 @@
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args){
+        //create scanner
+
+        //prompt user
+
+        //create fileChange obj
+
+        //call fileChange
+
+    }
+
+}
